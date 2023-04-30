4f3a922861eb18de55c7fa613fea114487dfa2e6
JackQweata <dahil3@yandex.ru>
2023-04-30 19:45:10 +0300

    реализация полного функционала

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from src.operation import AccountTransactions
+from utils.receiving_invoices import get_invoice
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    list_operations = get_invoice()
+
+    for items in list_operations:
+        account = AccountTransactions(items)
+
+        print(f"{account.get_date()} "
+              f"{account.get_description()}\n{account.get_from_operation()} -> "
+              f"{account.get_to_operation()}\n{account.get_amount()}\n")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
