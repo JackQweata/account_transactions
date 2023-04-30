5c744708df4faa10c2210b2473edb7c2dd0df68f
JackQweata <dahil3@yandex.ru>
2023-04-30 20:48:36 +0300

    фильтрация по state

--- utils/receiving_invoices.py
+++ utils/receiving_invoices.py
@@ -5,5 +5,11 @@ import os
 def get_invoice():
     link_data = os.path.abspath('data/operations.json')
     with open(link_data, encoding='utf-8') as file:
-        response = json.loads(file.read())[-5:]
-        return response
+        response = json.loads(file.read())
+
+        list_executed = []
+        for item in response:
+            if item.get('state') == 'EXECUTED':
+                list_executed.append(item)
+
+        return list_executed[-5:]
